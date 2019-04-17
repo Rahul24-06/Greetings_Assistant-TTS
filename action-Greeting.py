2911432185c132d6f37f3f47f4e20d3822f932d1
the lonely programmer <rahulkhanna24.06@gmail.com>
2019-04-18 01:08:14 +0530

    Update action-Greeting.py

--- action-Greeting.py
+++ action-Greeting.py
@@ -8,27 +8,21 @@ MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
-hi_choice = ['hello', 'howdy', 'hi', 'greetings']
-bye_choice = ['bye', 'see you later', 'good bye', 'bye now']
-
 def intent_received(hermes, intent_message):
     sentence = ''
 
     if intent_message.intent.intent_name == 'SirBuildsALot7:hello':
-        say = random.choice(hi_choice)
-		print(say)
-        sentence = say
+	print('hi')
+        sentence = 'Hello Rahul'
     elif intent_message.intent.intent_name == 'SirBuildsALot7:bye':
-        say = random.choice(bye_choice)
-		print(say)
-        sentence = say
+        print('bye')
+        sentence = 'See you Rahul'
     else:
         return
 
     hi_slot = intent_message.slots.hello.first()
     bye_slot = intent_message.slots.bye.first()
     
-    sentence += 'Rahul'
     hermes.publish_end_session(intent_message.session_id, sentence)
 
 
