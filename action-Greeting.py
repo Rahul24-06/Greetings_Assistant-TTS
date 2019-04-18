c79d42da003364f006391997540808055945e58e
the lonely programmer <rahulkhanna24.06@gmail.com>
2019-04-19 00:27:30 +0530

    Update action-Greeting.py

--- action-Greeting.py
+++ action-Greeting.py
@@ -1,30 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
+import random
 from hermes_python.hermes import Hermes
 
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
+hi_choice = ['hello', 'howdy', 'hi', 'greetings']
+bye_choice = ['bye', 'see you later', 'good bye', 'bye now']
+
 def intent_received(hermes, intent_message):
-    sentence = ''
+sentence = ''
 
-    if intent_message.intent.intent_name == 'SirBuildsALot7:hello':
-	print('hi')
-        sentence = 'Hello Rahul'
-    elif intent_message.intent.intent_name == 'SirBuildsALot7:bye':
-        print('bye')
-        sentence = 'See you Rahul'
-    else:
-        return
+if intent_message.intent.intent_name == 'SirBuildsALot7:hello':
+	say = random.choice(hi_choice) + 'Rahul'
+	print(say)
+	sentence += say
+elif intent_message.intent.intent_name == 'SirBuildsALot7:bye':
+	say = random.choice(bye_choice) + 'Rahul'
+	print(say)
+	sentence += say
+else:
+	return
 
-#    hi_slot = intent_message.slots.hello.first()
-#    bye_slot = intent_message.slots.bye.first()
-    
-    hermes.publish_end_session(intent_message.session_id, sentence)
+#sentence += 'Rahul'
+hermes.publish_end_session(intent_message.session_id, sentence)
 
 
 with Hermes(MQTT_ADDR) as h:
-    h.subscribe_intents(intent_received).start()
+h.subscribe_intents(intent_received).start()
